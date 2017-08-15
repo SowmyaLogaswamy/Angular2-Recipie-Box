1a20dac93e01d60e7542af0bc7a642a908ed45ab
Sowmya Logaswamy <sowmyalogaswamy@gmail.com>
2017-08-15 16:44:04 -0700

    Add ngFor and ngIf

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,8 +9,29 @@ import { Component } from '@angular/core';
       <h3>{{subtitle}} done on {{month}}/{{day}}/{{year}}</h3>
     </div>
     <ul>
-       <li *ngFor="let currentRecipe of recipes">{{currentRecipe.title}}</li>
+       <li [class]="priorityColor(currentRecipe)" (click)="isDone(currentRecipe)" *ngFor="let currentRecipe of recipes">{{currentRecipe.title}}
+       <button (click)="editRecipe(currentRecipe)">Edit</button></li>
      </ul>
+
+     <hr>
+      <div>
+       <div *ngIf ="selectedRecipe">
+         <h3>{{selectedRecipe.title}}</h3>
+         <p>Recipe Finished? {{selectedRecipe.done}}</p>
+         <hr>
+
+
+         <h3>Edit Recipe</h3>
+         <label>Enter Recipe Title:</label>
+         <input [(ngModel)]="selectedRecipe.title">
+         <label>Select Recipe Priority (1-3):</label>
+         <br>
+         <input type="radio" [(ngModel)]="selectedRecipe.priority" [value]="1">1 (Low Priority)<br>
+         <input type="radio" [(ngModel)]="selectedRecipe.priority" [value]="2">2 (Medium Priority)<br>
+         <input type="radio" [(ngModel)]="selectedRecipe.priority" [value]="3">3 (High Priority)
+         <button (click)="finishedEditing()">Done</button>
+         </div>
+      </div>
   </div>
   `
 })
@@ -23,14 +44,41 @@ export class AppComponent {
   year: number = this.currentTime.getFullYear();
 
   recipes: Recipe[] = [
-    new Recipe('Mushroom cheese dosa'),
-    new Recipe("Mushroom cheese dosa"),
-    new Recipe("Mushroom cheese dosa"),
-  ];
+    new Recipe('Mushroom cheese dosa', 3),
+    new Recipe("Cheese Pasta", 2),
+    new Recipe("Chilly Manchurian", 1) ];
+  selectedRecipe: null;
+
+
+  editRecipe(clickedRecipe) {
+       this.selectedRecipe = clickedRecipe;
+  }
+
+  isDone(clickedRecipe: Recipe) {
+    if(clickedRecipe.done === true) {
+      alert("This recipe is done!");
+    } else {
+      alert("This recipe is not done. Better get to work!");
+    }
+  }
+
+  priorityColor(currentRecipe){
+    if (currentRecipe.priority === 3){
+      return "bg-danger";
+    } else if (currentRecipe.priority === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
+
+  finishedEditing() {
+    this.selectedRecipe = null;
+  }
 }
 
 //Creating a model
 export class Recipe {
   public done: boolean = false;
-  constructor(public title: string) { }
+  constructor(public title: string, public priority: number) { }
 }
